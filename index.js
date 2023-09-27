266682a30779a0c77ca6ae4457a61ad4111c5fba
Replit user <>
2023-09-27 14:04:03 +0000

    Added the questions & multiple choice anwsers

---
+++ index.js
@@ -0,0 +1,75 @@
+const quizData = [
+  {
+    question: 'What is the capital of India',
+    options: ['New Delhi', 'London', 'Newyork', 'Beijing'],
+    answer: 'New Delhi',
+  },
+  {
+    question: 'When was Netflix founded?',
+    options: ['1997', '2001', '2009', '2015'],
+    answer: '1997',
+  },
+  {
+    question: 'Which country won the FIFA World Cup in 2018?',
+    options: ['Brazil', 'Germany', 'France', 'Argentina'],
+    answer: 'France',
+  },
+  {
+    question: 'What is the tallest mountain in the world?',
+    options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+    answer: 'Mount Everest',
+  },
+  {
+    question: 'What’s the national flower of Japan?',
+    options: [
+      'Lilly',
+      'Sunflower',
+      'Cherry blossom',
+      'Rose',
+    ],
+    answer: 'Cherry blossom',
+  },
+  {
+    question: 'What is the chemical symbol for iron?',
+    options: ['Au', 'Fe', 'Cu', 'H'],
+    answer: 'Fe',
+  },
+  {
+    question: 'How many time zones are there in Russia?',
+    options: [
+      '11',
+      '15',
+      '1',
+      '7',
+    ],
+    answer: '11',
+  },
+  {
+    question: 'How many keys does a classic piano have?',
+    options: ['66', '55', '44', '88'],
+    answer: '88',
+  },
+  {
+    question: 'Which football team is known as ‘The Red Devils’?',
+    options: [
+      'FC Bayen Munich',
+      'Manchester City F.C.',
+      'Manchester United F.C.',
+      'Liverpool F.C.',
+    ],
+    answer: 'Whale Shark',
+  },
+  {
+    question: 'Which animal has genetics almost similar to that of the human?',
+    options: ['Lion', 'Rat', 'Elephant', 'Giraffe'],
+    answer: 'Rat',
+  },
+];
+
+
+const quizContainer = document.getElementById('question');
+const resultContainer = document.getElementById('manswer');
+
+
+
+
